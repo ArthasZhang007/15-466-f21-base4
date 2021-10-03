4c3700db26c7711f3eb7b9f30c0b073ce67db0fe
ArthasZhang007 <a1103753961@gmail.com>
2021-10-02 20:11:52 -0400

    oops update

--- oops.cpp
+++ oops.cpp
@@ -70,17 +70,36 @@ ostream &operator <<(ostream &os, State S){
 State id_state{
     "id"
 };
-State id_message(std::string mes){
+void message(std::string mes){
     std::cout<<mes<<"\n\n";
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+}
+State id_message(std::string mes){
+    message(mes);
     return id_state;
 }
 
+State game_over;
+State bad_endding1, bad_endding2, bad_endding3;
+State good_endding;
+State east_room_1F, north_room_1F;
+State middle_state_1F;
+State start_state;
 
-State game_over{
-    "Game is over, try again!"
+
+
+void state_init(){
+game_over = {
+    "Game is over, try again!",
+    {{
+        "Restart!",
+        [](atr_type &atr){
+            atr.clear();
+            return start_state;
+        }
+    }}
 };
-State bad_endding1{
+bad_endding1 = {
     "After trembling for hours in the freezing unknown place...... you are frozen to death."
     ,
     {{
@@ -88,15 +107,32 @@ State bad_endding1{
         [](atr_type &atr){return game_over;}
     }}
 };
-State east_room_1F{};
-State north_room_1F{
-    "You pushed the door and found it ...... broken and crashed into fragments on the floor"
-    "A nice artistic portrait of some ...... king is in front of you. He is on a horse, using a long sword cutting through the throat of evil enemy"
-    "his soldiers are motivated by this greatly and start to fight as well, even if the enemies are strong."
-    "Now on the left side of the portrait "
+bad_endding2 = {
+    "A sharp sword comes from the mirror and cut through your throat!"
+    ,
+    {{
+        "Game Over!",
+        [](atr_type &atr){return game_over;}
+    }}
 };
-
-State middle_state_1F{
+start_state = {
+    "You wake up from a long dream, it feels like a million year has passed. "
+    "Everything around you is pure darkness"
+    "Now you feel a warm thing in your hand..... it seems like a button, do you want to push it?"
+    ,
+    {
+        {
+            "Yes",
+            [](atr_type &atr){return middle_state_1F;}
+        }
+        ,
+        {
+            "No",
+            [](atr_type &atr){return bad_endding1;}
+        }
+    }
+};
+middle_state_1F = {
     "Suddenly, multiple electric lights are on.... actually, wait. These are just kerosene lamps."
     "That's why the room is still so dim. Is this a mansion? prison? paradise? British classroom?"
     "You have absolutely no clue. The only thing you can see is that there are 8 doors around you, in 0', 45', 90' 135' 180' 225' "
@@ -129,38 +165,76 @@ State middle_state_1F{
         },
         {
             "135",
+            [](atr_type &atr){
+                return id_message("This is a fake room");
+            }
         },
         {
             "180",
+            [](atr_type &atr){
+                if(atr["east_room"] == "open"){
+                    return east_room_1F;
+                }
+                return id_message("This is a fake room");
+            }
         },
         {
             "225",
+            [](atr_type &atr){
+                return id_message("This is a fake room");
+            }
         },
         {
             "270",
+            [](atr_type &atr){
+                return id_message("This is a fake room");
+            }
         },
         {
             "315",
+            [](atr_type &atr){
+                return id_message("This is a fake room");
+            }
         }
     }
 };
-State start_state{
-    "You wake up from a long dream, it feels like a million year has passed. "
-    "Everything around you is pure darkness"
-    "Now you feel a warm thing in your hand..... it seems like a button, do you want to push it?"
-    ,
+
+north_room_1F = {
+    "You pushed the door and found it ...... broken and crashed into fragments on the floor. "
+    "A nice artistic portrait of some ...... king is in front of you. He is on a horse, using a long sword cutting through the throat of evil enemy. "
+    "His soldiers are motivated by this greatly and start to fight as well, even if the enemies are strong. "
+    "Now on the left side of the portrait, there is a mirror, on the right side there is another one. The left is decorated with diamonds and gold "
+    "with a cross on it, the right one is made by simple wood and has a demon symbol on it, what do you want to do next? ",
     {
         {
-            "Yes",
-            [](atr_type &atr){return middle_state_1F;}
-        }
-        ,
+            "look at the left mirror",
+            [](atr_type &atr){
+                return bad_endding2;
+            }
+        },
         {
-            "No",
-            [](atr_type &atr){return bad_endding1;}
+            "look at the right mirror",
+            [](atr_type &atr){
+                atr["east_room"] = "open";
+                message("You see a handsome boy with a great cowboy hat in the mirror, isn't it cool?");
+                return id_message("You hear a crack sound from outside!");
+            } 
+        },
+        {
+            "exit the room",
+            [](atr_type &atr){
+                return middle_state_1F;
+            } 
         }
     }
 };
+
+east_room_1F = {
+    "It is so weird that door opens automatically, while just few minutes ago the door is as hard as a rock"
+};
+
+}
+
 
 
 
@@ -248,6 +322,7 @@ void init(){
     
 }
 int main(){
+    state_init();
     init();
     return 0;
 }
